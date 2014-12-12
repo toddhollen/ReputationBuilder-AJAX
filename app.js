74835785bc2282eb9bc5af68283a93d1a87ec1dd
toddhollen <toddhollen@gmail.com>
2014-12-12 13:31:40 -0700

    Updated

--- app.js
+++ app.js
@@ -20,39 +20,6 @@ $(document).ready( function() {
 
 });
 
-// this function takes the question object returned by StackOverflow 
-// and creates new result to be appended to DOM
-var showQuestion = function(question) {
-	
-	// clone our result template code
-	var result = $('.templates .question').clone();
-	
-	// Set the question properties in result
-	var questionElem = result.find('.question-text a');
-	questionElem.attr('href', question.link);
-	questionElem.text(question.title);
-
-	// set the date asked property in result
-	var asked = result.find('.asked-date');
-	var date = new Date(1000*question.creation_date);
-	asked.text(date.toString());
-
-	// set the #views for question property in result
-	var viewed = result.find('.viewed');
-	viewed.text(question.view_count);
-
-	// set some properties related to asker
-	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-		question.owner.display_name +
-		'</a>' +
-		'</p>' +
-		'<p>Reputation: ' + question.owner.reputation + '</p>'
-	);
-
-	return result;
-};
-
 
 // this function takes the results object from StackOverflow
 // and creates info about search results to be appended to DOM
@@ -101,38 +68,76 @@ var getUnanswered = function(tags) {
 	});
 };
 
+// this function takes the question object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showQuestion = function(question) {
+	
+	// clone our result template code
+	var result = $('.templates .question').clone();
+	
+	// Set the question properties in result
+	var questionElem = result.find('.question-text a');
+	questionElem.attr('href', question.link);
+	questionElem.text(question.title);
+
+	// set the date asked property in result
+	var asked = result.find('.asked-date');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the #views for question property in result
+	var viewed = result.find('.viewed');
+	viewed.text(question.view_count);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+		question.owner.display_name +
+		'</a>' +
+		'</p>' +
+		'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	return result;
+};
+
 var getAnswerer = function(tags) {
 	
 	var result = $.ajax({
 		url: "http://api.stackexchange.com/2.2/tags/"+tags+"/top-answerers/month?site=stackoverflow",
-		data: request,
 		dataType: "jsonp",
-		type: "GET",
-		})
+		type: "GET"
+	})
+	.done( function(result){
+		
+		$.each(result.items, function(i, person){
+			var answerer = showAnswerer(person);
+			$('.results').append(answerer);
+		});
 
-	// 	// Need to create a new function here to create 
-	// 	// a showAnswerer fuction that works like the showQuestion function above. 
-
-	var showAnswerer = function(answerers) {
-	
-	// clone our result template code
-	var result = $('.templates .answerer').clone();
-	
-	// set the link display name and profile property in result
-	var profile = result.find('.profile');
-	answererElem.attr('href', answerer.link);
-	answererElem.text(answerer.display_name);
-	);
-
-	// set the reputaiton for answerer property in result
-	var viewed = result.find('.reputaton');
-	viewed.text(question.reputaton);
-
-	return result;
-};
+	})
 	.fail(function(jqXHR, error, errorThrown){
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
 	});
 };
 
+	// 	// Need to create a new function here to create 
+	// 	// a showAnswerer fuction that works like the showQuestion function above. 
+
+var showAnswerer = function(answerers) {
+
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+	
+	// set the link display name and profile property in result
+	var profile = result.find('.profile a');
+	answererElem.attr('href', answerer.link);
+	answererElem.text(answerer.display_name);
+
+	// set the reputaiton for answerer property in result
+	var viewed = result.find('.reputaton');
+	viewed.text(question.reputaton);
+
+	return result;
+}
